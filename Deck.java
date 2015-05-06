fb17d44939fdfef03482baf8ea7a71776b943898
Ethan Wunschel <ethanwun@gmail.com>
2015-05-06 09:46:03 -0700

    Added Deck Creation Functionality

--- Deck.java
+++ Deck.java
@@ -7,6 +7,20 @@ public class Deck {
    public Deck(){
       deck = new ArrayList<Card>();
       //TODO: Add a card (2-14) of each suit (h,s,d,c)
+      for(int x = 1; x < 5; x++)
+      {
+        for(int i = 2; i < 15; i++)
+        {
+          if(x==1)
+            deck.add(new Card(i,"h"));
+          if(x==2)
+            deck.add(new Card(i, "s"));
+          if(x==3)
+            deck.add(new Card(i, "d"));
+          if(x==4)
+            deck.add(new Card(i, "c"));
+        }
+      }
    }
 
    public Card remove(){
